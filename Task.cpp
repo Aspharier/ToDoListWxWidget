53ad20e380d255a51bb36ea0a4eb40909a4131c2
Aspharier <codeworkashish@gmail.com>
2024-06-29 17:26:55 +0530

    Add project files.

---
+++ Task.cpp
@@ -0,0 +1,12 @@
+#include "Task.h"
+#include <string>
+#include <vector>
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName)
+{
+}
+
+std::vector<Task> loadTasksFromFile(const std::string& fileName)
+{
+	return std::vector<Task>();
+}
