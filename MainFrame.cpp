d3f8eed7d32d007b4a56ab52c905352ee06aadff
Aspharier <codeworkashish@gmail.com>
2024-06-30 18:39:02 +0530

    First commit

---
+++ MainFrame.cpp
@@ -0,0 +1,9 @@
+#include "MainFrame.h"
+#include <wx/wx.h>
+#include <string>
+#include <vector>
+#include "Task.h"
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr,wxID_ANY,title)
+{
+}
