8faf1ae788cb0555c9f4d1cee544bd0d99050c64
Aspharier <codeworkashish@gmail.com>
2024-07-01 17:47:47 +0530

    Second Commit

--- MainFrame.h
+++ MainFrame.h
@@ -5,5 +5,18 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+private:
+	void CreateControls();
+	void BindEventHandlers();
+
+	void OnAddButtonClicked(wxCommandEvent& evt);
+	void AddTaskFromInput();
+
+	wxPanel* panel;
+	wxStaticText* headlineText;
+	wxTextCtrl* inputField;
+	wxButton* addButton;
+	wxCheckListBox* checkListBox;
+	wxButton* clearButton;
 };
 
